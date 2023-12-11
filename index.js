230e7dea3bcc1826343db74d14d3b88ebd333e24
Guruju Akhila <125969582+GurujuAkhila@users.noreply.github.com>
2023-12-11 18:14:32 +0530

    Update index.js img ../ to ./

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ const apiKey = 'f342f49b350e6359215f709364f82679'
                 document.querySelector(".wind").innerHTML = data.wind.speed + "kmph";
 
                 if (data.weather[0].main === "Clouds") {
-                    weatherIcon.src = "../images/clouds.png"
+                    weatherIcon.src = "./images/clouds.png"
                 }
                 else if (data.weather[0].main === "Clear") {
-                    weatherIcon.src = '../images/clear.png';
+                    weatherIcon.src = '/images/clear.png';
                 }
                 else if (data.weather[0].main === "Rain") {
-                    weatherIcon.src = '../images/rain.png';
+                    weatherIcon.src = './images/rain.png';
                 }
                 else if (data.weather[0].main === "Drizzle") {
-                    weatherIcon.src = '../images/drizzle.png';
+                    weatherIcon.src = './images/drizzle.png';
                 }
                 else if (data.weather[0].main === "Mist") {
-                    weatherIcon.src = '../images/mist.png';
+                    weatherIcon.src = './images/mist.png';
                 }
             
                 document.querySelector('.weather').style.display = 'block';
